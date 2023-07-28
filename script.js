7cf186bad392972c9ca60e425a27b581e5f629e4
Bada Jesutobi <jbada467@stu.ui.edu.ng>
2023-07-28 02:22:40 -0700

    Theme-change activated

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ theme_switch.addEventListener('click', ()=>{
     if (theme_name.innerHTML == "Dark"){
         theme_name.innerHTML = "Light";
         icon_img.src = "./images/sun.svg";
+        document.body.classList.toggle("dark-theme");
     }
     else{
         theme_name.innerHTML = "Dark";
         icon_img.src = "./images/moon.svg";
+        document.body.classList.toggle("dark-theme");
     }
   })
